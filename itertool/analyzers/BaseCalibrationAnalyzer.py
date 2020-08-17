560fbc915ba62ea74d611c7e666c17ab9d047fbc
zdu <zdu@idmod.org>
2020-08-17 07:19:52 -0700

    implement BaseCalibrationAnalyzer

---
+++ itertool/analyzers/BaseCalibrationAnalyzer.py
@@ -0,0 +1,16 @@
+import json
+
+from idmtools.entities import IAnalyzer
+# from simtools.Analysis.BaseAnalyzers import BaseAnalyzer
+from itertool.utilities.Encoding import GeneralEncoder
+
+
+class BaseCalibrationAnalyzer(IAnalyzer):
+    def __init__(self, uid=None, working_dir=None, parse=True, need_dir_map=False, filenames=None,
+                 reference_data=None, weight=1):
+        super().__init__(uid=uid, working_dir=working_dir, parse=parse, filenames=filenames)
+        self.reference_data = reference_data
+        self.weight = weight
+
+    def cache(self):
+        return json.dumps(self, cls=GeneralEncoder)
