4c4eece4f0ac4bc0a7831bc5f794a90edd5ac028
Jen Schripsema <jschripsema@idmod.org>
2020-08-28 18:08:41 -0600

    updated RTD YAML file to treat warnings as errors and small fixes to generate-api in makefiles

--- docs/make.bat
+++ docs/make.bat
@@ -47,6 +47,7 @@ if "%1" == "help" (
 if "%1" == "clean" (
 	for /d %%i in (%BUILDDIR%\*) do rmdir /q /s %%i
 	del /q /s %BUILDDIR%\*
+	del itertool*.rst modules.rst >nul 2>&1
 	goto end
 )
 
@@ -77,6 +78,7 @@ if "%1" == "generate-api" (
     set SPHINX_APIDOC_OPTIONS=members,undoc-members,show-inheritance,ignore-module-all
 
     del modules.rst >nul 2>&1
+    del itertool_index.rst >nul 2>&1
     sphinx-apidoc -f -e -o . ../itertool
 	REN modules.rst itertool_index.rst
 	goto end
