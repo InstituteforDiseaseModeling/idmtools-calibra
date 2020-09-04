38bac44aab1dcd1eee25b5b61d6a1e460fe3489f
zdu <zdu@idmod.org>
2020-09-04 07:12:25 -0700

    remove dtk-tools dependence from malaria params.py

--- malaria/params.py
+++ malaria/params.py
@@ -1,7 +1,7 @@
 import copy
 
 from malaria import infection, immunity, symptoms
-from dtk.vector.species import set_params_by_species
+# from dtk.vector.species import set_params_by_species
 from malaria.interventions.malaria_drugs import drug_params
 
 
