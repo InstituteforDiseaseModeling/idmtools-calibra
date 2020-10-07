9cb2c312f417c0436764e8b7cafad5c3677d1d4a
zdu <zdu@idmod.org>
2020-10-07 06:07:01 -0700

    add utility function to support synamic model config

--- itertool/utilities/helper.py
+++ itertool/utilities/helper.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 from emodpy.utils import download_latest_bamboo, EradicationBambooBuilds
-from emod_api.config import default_from_schema_no_validation as dfs
 from emod_api.schema import get_schema as gs
 
 max_exp_name_len = 255
@@ -27,6 +26,8 @@ def generate_default_config_from_exe(local_dir, plan=EradicationBambooBuilds.CI_
     Returns: exe_path, config_path
 
     """
+    from emod_api.config import default_from_schema_no_validation as dfs
+
     exe_path = os.path.join(local_dir, "Eradication.exe")
     if not os.path.exists(exe_path):
         eradication_path_bamboo = download_latest_bamboo(
@@ -45,5 +46,38 @@ def generate_default_config_from_exe(local_dir, plan=EradicationBambooBuilds.CI_
     dfs.write_default_from_schema(schema_path)
     # use our file name
     shutil.move("default_config.json", config_path)
+
+    return exe_path, config_path
+
+
+def generate_model_config_from_exe(local_dir, plan=EradicationBambooBuilds.CI_GENERIC, model="MALARIA_SIM"):
+    """
+    Check and down;oad Eradication.exe from bamboo, generate schema and default config file
+    Args:
+        local_dir: local folder to contain Eradication.exe
+        plan: enum EradicationBambooBuilds
+        model: config model
+
+    Returns: exe_path, config_path
+
+    """
+    from emod_api.config import from_schema as fs
+
+    exe_path = os.path.join(local_dir, "Eradication.exe")
+    if not os.path.exists(exe_path):
+        eradication_path_bamboo = download_latest_bamboo(
+            plan=plan,
+            scheduled_builds_only=False
+        )
+        # print(eradication_path_bamboo)
+        shutil.move(eradication_path_bamboo, exe_path)
+
+    schema_path = os.path.join(local_dir, f"{plan.name}_schema.json")
+    config_path = os.path.join(local_dir, f"{plan.name}_config.json")
+
+    # generate schema
+    gs.dtk_to_schema(exe_path, path_to_write_schema=schema_path)
+    # use our file name
+    fs.SchemaConfigBuilder(schema_name=schema_path, config_out=config_path, model=model)
 
     return exe_path, config_path
