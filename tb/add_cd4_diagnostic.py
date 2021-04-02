df6f402d4dea02faf04bc62f3bae1619829c14b4
shchen <shchen@idmod.org>
2021-04-01 17:02:17 -0700

    change brad example folder name to tbhiv_calibra

--- tb/add_cd4_diagnostic.py
+++ tb/add_cd4_diagnostic.py
@@ -65,4 +65,4 @@ def add_cd4_diagnostic(task, trigger_treatment_list, event_200='Below200', event
         if event not in existing_event_list and event is not None:
             existing_event_list.append(event)
 
-    task.set_parameter('Listed_Events', existing_event_list)
+    task.config.parameters.Listed_Events = existing_event_list
