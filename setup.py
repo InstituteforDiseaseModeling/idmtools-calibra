b2886361d2980899f3e2a01b0b6963d7688271f6
zdu <zdu@idmod.org>
2021-07-22 11:32:21 -0700

    add entry point for resume cli command

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     include_package_data=True,
     keywords='modeling, IDM',
     name='idmtools-calibra',
+    entry_points={"idmtools_cli.cli_plugins": ["calibra=idmtools_calibra.cli.commands:calibra"]},
     packages=find_packages(),
     setup_requires=setup_requirements,
     python_requires='>=3.6.*, !=3.7.0, !=3.7.1, !=3.7.2, <3.9',
