4f0c96791c2e73419b7b12c75a315aabe81d7490
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2022-03-07 14:50:10 -0800

    update python version to 3.9

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     entry_points={"idmtools_cli.cli_plugins": ["calibra=idmtools_calibra.cli.commands:calibra"]},
     packages=find_packages(),
     setup_requires=setup_requirements,
-    python_requires='>3.6.*, !=3.7.0, !=3.7.1, !=3.7.2, <=3.9',
+    python_requires='>3.6.*, !=3.7.0, !=3.7.1, !=3.7.2, <=3.9.*',
     test_suite='tests',
     extras_require=extras,
     version='1.0.6'
