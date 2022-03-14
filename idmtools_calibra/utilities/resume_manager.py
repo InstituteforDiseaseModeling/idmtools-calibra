a5bd9891421484ddf80fe56c1cd5216cc55b6510
zdu <zhaowei.du@gatesfoundation.org>
2022-03-14 11:21:26 -0700

    fix restore ll_all

--- idmtools_calibra/utilities/resume_manager.py
+++ idmtools_calibra/utilities/resume_manager.py
@@ -265,7 +265,7 @@ class ResumeManager(object):
             os.remove(ll_all_path)
 
         if self.iteration > 0:
-            if self.iter_step.value < StatusPoint.plot.value:
+            if self.iter_step.value <= StatusPoint.plot.value:
                 generate_ll_all(self.calib_manager, iteration=self.iteration - 1, ll_all_name=ll_all_name)
             else:
                 generate_ll_all(self.calib_manager, iteration=self.iteration, ll_all_name=ll_all_name)
