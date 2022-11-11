75c7c931706fef36e7d57085f8314bcff7c23a1e
Jonathan Bloedow <jhb4@cornell.edu>
2022-11-11 15:18:57 -0800

    7 iterations.

--- examples/emod_sir/OutputOption1/settings.py
+++ examples/emod_sir/OutputOption1/settings.py
@@ -20,7 +20,7 @@ class Settings:
     # number of randomly seeded simulations per parameter set/sample
     N_REPLICATES = 1 # 5
     # the number of times the algorithm will attempt to optimize the best-guess parameterization
-    N_ITERATIONS = 5
+    N_ITERATIONS = 7
     # Calibration state/results will be kept in a directory by this name in the same directory as this file
     CALIBRATION_NAME = 'emod-sir'
 
