555e16590ac281b264fe82c5d8ab9558e898ee76
Jonathan Bloedow <jhb4@cornell.edu>
2022-11-22 12:01:56 -0800

    Slight refactor of linear model to be able to run more easily from notebook.

--- examples/solar/bin/linear_model.py
+++ examples/solar/bin/linear_model.py
@@ -25,18 +25,7 @@ def parse_args():
     return args
 
 
-def main(args):
-    # load the config file
-    with open(args.config_filename, 'r') as f:
-        config = json.load(f)
-
-    # set the random seed
-    run_number = int(config['Run_Number'])
-    random.seed(run_number)
-
-    # generate results for x = 1 through 200
-    a = config['a']
-    b = config['b']
+def run_from_values( a, b ):
     independent_values = [x+1 for x in range(200)]
     model_results = [linear_model(a, b, x+1) for x in independent_values]
 
@@ -49,8 +38,21 @@ def main(args):
     output_path = Path('output', 'production.csv')
     output_path.parent.mkdir(parents=True, exist_ok=True)
     with open(output_path, 'w') as f:
-        f.write(lines_str)
+        f.write(lines_str) 
 
+def main(args):
+    # load the config file
+    with open(args.config_filename, 'r') as f:
+        config = json.load(f)
+
+    # set the random seed
+    run_number = int(config['Run_Number'])
+    random.seed(run_number)
+
+    # generate results for x = 1 through 200
+    a = config['a']
+    b = config['b']
+    run_from_values( a, b )
 
 if __name__ == '__main__':
     args = parse_args()
