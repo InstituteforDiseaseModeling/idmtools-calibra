561cfa6a1b465bd4dc3897847825827e8e044913
Jonathan Bloedow <jhb4@cornell.edu>
2023-01-26 13:43:44 -0800

    Cleaning up some examples, even if not being showcased at this time.

--- examples/emod_sir/OutputOption2/calibrate.py
+++ examples/emod_sir/OutputOption2/calibrate.py
@@ -5,79 +5,8 @@ from idmtools_calibra.rmse_site import RMSESite
 from idmtools_calibra import calib_base_app as calib_app
 
 import manifest 
-
-class Settings:
-    #
-    # Run environment controls
-    #
-    LOCALE = 'CALCULON'
-    MODEL_DRIVER = manifest.eradication_path
-    #CONFIG_FILENAME = 'config.json'
-    REFERENCE_DATA_DIR = manifest.REFERENCE_DATA_DIR 
-    INPUT_DIRS = []
-    #SIF = "0f228554-04c4-eb11-a9ec-b88303911bc1" # 'dtk_centos.id'
-    SIF = manifest.sif
-    #SIF_FILENAME = "dtk_centos.sif"
-    SIF_FILENAME = manifest.sif_filename
-
-    #
-    # Calibration controls
-    #
-
-    # The number of parameter sets/samples to run in each calibration iteration
-    N_SAMPLES = 125
-    # number of randomly seeded simulations per parameter set/sample
-    N_REPLICATES = 1 # 5
-    # the number of times the algorithm will attempt to optimize the best-guess parameterization
-    N_ITERATIONS = 10
-    # Calibration state/results will be kept in a directory by this name in the same directory as this file
-    CALIBRATION_NAME = 'emod-sir-2'
-
-    """
-    Calibration parameter specification
-
-    Dynamic parameters are those that calibra can use to explore parameter space during calibration, 
-        non-dynamic parameters are simply overrides of model parameters.
-
-    Name: Human readable name of the parameter to utilize
-    Dynamic: True/False, whether this parameter can be altered by the calibration (False still overrides input files)
-    MapTo: actual model parameter name to use. If not specified, the map_sample_to_model_input method below will 
-        have to handle the (more complicated) mapping of this parameter to actual model parameter(s)
-    Guess: Initial value for parameter in calibration
-    Min: The minimum value the parameter can be in the calibration (if Dynamic is True) (required even if not Dynamic)
-    Max: The maximum value the parameter can be in the calibration (if Dynamic is True) (required even if not Dynamic)
-
-    """
-    CALIBRATION_PARAMETERS = [
-        {
-            'Name': 'a',
-            'Dynamic': True,
-            'MapTo': 'Base_Infectivity_Constant',
-            'Guess': 5,
-            'Min': 0,
-            'Max': 10.0
-        },
-        {
-            'Name': 'b',
-            'Dynamic': True,
-            'MapTo': 'Infectious_Period_Exponential',
-            'Guess': 5,
-            'Min': 0.1,
-            'Max': 10
-        },
-        {
-            'Name': 'c',
-            'Dynamic': True,
-            'MapTo': 'Incubation_Period_Constant',
-            'Guess': 5,
-            'Min': 0,
-            'Max': 10
-        }
-    ]
-    volume_fraction = 0.0002
-    num_to_plot = 5
-
-settings = Settings()
+from settings import Settings
+mysettings = Settings()
 
 if __name__ == "__main__":
     import matplotlib
@@ -85,7 +14,7 @@ if __name__ == "__main__":
     # site we want to calibrate on - a core organization object for calibra
     site = RMSESite(
         name='rmse_site',
-        reference_sources={'production': os.path.join(settings.REFERENCE_DATA_DIR, 'output.csv')}
+        reference_sources={'production': os.path.join(mysettings.REFERENCE_DATA_DIR, 'output.csv')}
     )
     from emodpy.emod_task import EMODTask
     def set_param_fn( config ):
@@ -115,18 +44,18 @@ if __name__ == "__main__":
     import emod_generic.bootstrap as dtk
     dtk.setup( manifest.model_dl_dir )
     from idmtools.core.platform_factory import Platform
-    platform = Platform(settings.LOCALE, node_group="idm_48cores", priority="AboveNormal")
+    platform = Platform(mysettings.LOCALE, node_group="idm_48cores", priority="AboveNormal")
     task = EMODTask.from_default2(
         config_path="config.json",
-        eradication_path=settings.MODEL_DRIVER,
+        eradication_path=mysettings.MODEL_DRIVER,
         campaign_builder=build_camp,
         demog_builder=None,
         schema_path=manifest.schema_file,
         param_custom_cb=set_param_fn,
         ep4_path=manifest.ep4
     )
-    task.set_sif( settings.SIF )
-    calib_man = calib_app.init( settings, site, task )
+    task.set_sif( mysettings.SIF )
+    calib_man = calib_app.init( mysettings, site, task )
     calib_man.platform = platform
 
     # Required variable/dict in calibration scripts
@@ -135,5 +64,5 @@ if __name__ == "__main__":
     }
     calib_app.go( calib_man )
 
-    #import bin.sir as sir
-    #sir.run_compare()
+    import test_and_plot as tap
+    tap.test_and_plot()
