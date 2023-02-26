d9f4905a5bbf730df4cc2937e68e79c994551c6f
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2023-02-25 16:59:58 -0800

    Add weight test

--- tests/integration/test_emod_hint.py
+++ tests/integration/test_emod_hint.py
@@ -4,6 +4,7 @@ import os
 import unittest
 from functools import partial
 
+import numpy as np
 import pandas as pd
 
 from idmtools_calibra import calib_base_app as calib_app
@@ -25,7 +26,7 @@ mysettings = settings.Settings()
 
 
 class TestEMODHint(unittest.TestCase):
-    def demog_mapper(build_demog_actual, mapto_key, value):
+    def demog_mapper(self, build_demog_actual, mapto_key, value):
         """
         This callback function maps paraemters names for the demogaign from calibra strings to
         the actual 'build_demog' function parameter names. We hook it up in the main function. See:
@@ -47,38 +48,35 @@ class TestEMODHint(unittest.TestCase):
             raise ValueError(f"{demog_fn_param} is not a valid calibration target.")
         return build_demog_actual
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        # site we want to calibrate on - a core organization object for calibra
-        cls.site = RMSESite(
-            name='rmse_site',
-            reference_sources={'production': os.path.join(mysettings.REFERENCE_DATA_DIR, 'output.csv')}
-        )
-        cls.platform = Platform(mysettings.LOCALE, node_group="idm_48cores", priority="Highest")
-        task = get_task()
-        calib_man = calib_app.init(mysettings, cls.site, task, platform=cls.platform)
+    def setUp(self):
+        self.platform = Platform(mysettings.LOCALE, node_group="idm_48cores", priority="Highest")
 
-        cls.calibra_name = mysettings.CALIBRATION_NAME
+    #@classmethod
+    def run_calibra(self, site) -> None:
+        # site we want to calibrate on - a core organization object for calibra
+        site = site
+        #self.platform = Platform(mysettings.LOCALE, node_group="idm_48cores", priority="Highest")
+        task = get_task()
+        calib_man = calib_app.init(mysettings, site, task, platform=self.platform)
+
+        self.calibra_name = mysettings.CALIBRATION_NAME
 
         date = datetime.datetime.now()
         uniq_filename = str(date.date()) + '_' + str(date.time()).replace(':', '_')
         print(uniq_filename)
 
-        cls.directory = os.path.join(CURRENT_DIRECTORY, "emod_hint_calibra_result", uniq_filename)
+        self.directory = os.path.join(CURRENT_DIRECTORY, "emod_hint_calibra_result", uniq_filename)
         calib_app.campaign_builder_fn = model.build_camp
         calib_app.demog_builder_fn = model.build_demog
-        calib_app.demog_mapper = cls.demog_mapper
-        calib_app.go(calib_man, directory=cls.directory)
+        calib_app.demog_mapper = self.demog_mapper
+        calib_app.go(calib_man, directory=self.directory)
 
         # following few lines for debug
-        # uniq_filename = '2023-02-10_13_32_42.373032'
-        # cls.directory = os.path.join(CURRENT_DIRECTORY, "emod_hint_calibra_result", uniq_filename)
-        # calib_app.go(calib_man, directory=cls.directory, resume=True, iteration=3, iter_step='plot', loop=True)
-        cls.experiments = download_experiment_files(cls.platform, cls.directory, cls.calibra_name, "output.csv")
+        # uniq_filename = '2023-02-23_14_40_26.793206'
+        # self.directory = os.path.join(CURRENT_DIRECTORY, "emod_hint_calibra_result", uniq_filename)
+        # calib_app.go(calib_man, directory=self.directory, resume=True, iteration=2, iter_step='analyze', loop=False)
+        self.experiments = download_experiment_files(self.platform, self.directory, self.calibra_name, "output.csv")
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        delete_experiments(cls.experiments)
 
     def test_emod_hint_regression_fitness(self):
         """
@@ -88,7 +86,12 @@ class TestEMODHint(unittest.TestCase):
         Although automation can not tell which iteration to stop, but at least can validate top one is the best
         https://towardsdatascience.com/what-are-the-best-metrics-to-evaluate-your-regression-model-418ca481755b
         """
-        reference_dict = self.site.reference_dict['production']
+        site = RMSESite(
+            name='rmse_site',
+            reference_sources={'production': os.path.join(mysettings.REFERENCE_DATA_DIR, 'output.csv')}
+        )
+        self.run_calibra(site)
+        reference_dict = site.reference_dict['production']
         ll_all_path = os.path.join(self.directory, self.calibra_name, "_plots", "LL_all.csv")
         with open(ll_all_path, newline='') as f:
             reader = csv.reader(f)
@@ -97,7 +100,6 @@ class TestEMODHint(unittest.TestCase):
             index_total = data[0].index('total')
             index_sim = data[0].index('simid')
             rmse_list = []
-            distance_list = []
             for i in range(1, len(data) - 2):
                 # validate LL_all.csv is sorted by 'total' column
                 self.assertTrue(float(data[i][index_total]) >= float(data[i + 1][index_total]))
@@ -116,4 +118,39 @@ class TestEMODHint(unittest.TestCase):
             # validate rmse_list is sorted in ascend order(small to large)
             self.assertTrue(all(a <= b for a, b in zip(rmse_list, rmse_list[1:])))
 
+    def test_emod_hint_with_weights(self):
+        site = RMSESite(
+            name='rmse_site',
+            reference_sources={'production': os.path.join('emod_hint', 'reference', 'output_weights.csv')}
+        )
+        self.run_calibra(site)
+        reference_dict = site.reference_dict['production']
+        ll_all_path = os.path.join(self.directory, self.calibra_name, "_plots", "LL_all.csv")
+        with open(ll_all_path, newline='') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            # validate "total" of likelihood column is sorted
+            index_total = data[0].index('total')
+            index_sim = data[0].index('simid')
+            rmse_list = []
+            for i in range(1, len(data) - 2):
+                # validate LL_all.csv is sorted by 'total' column
+                self.assertTrue(float(data[i][index_total]) >= float(data[i + 1][index_total]))
 
+                # validate ll_all.csv is sorted with RMSE - Root Mean Square Error which means the top one is the best
+                # prediction against the real reference value
+                # first get simulation by id
+                sim_id = data[i][index_sim].replace("('", '').replace("',)", '')
+                # Get value 'column' in simulation's output.csv from comps
+                sim_output_value = get_output_data(self.experiments, sim_id, 'value')
+                # Calculate RMSE for each simulation with weights
+                # get raw weight
+                w = site.reference_dict['production']['weights']
+                # # normalized weights. looks like no need to normalized weight for mean_squared_error
+                # w *= (np.ones(len(w)))*(1/sum(w))
+                rmse = math.sqrt(mean_squared_error(sim_output_value, reference_dict['value_reference'], sample_weight=w))
+                # save each rmse value to a list
+                rmse_list.append(rmse)
+
+            # validate rmse_list is sorted in ascend order(small to large)
+            self.assertTrue(all(a <= b for a, b in zip(rmse_list, rmse_list[1:])))
