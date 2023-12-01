4cd8c5d8b2877d704f9bc7d2c10bfb5f2fa72c32
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2023-12-01 15:29:01 -0800

    Update version from version.py

--- setup.py
+++ setup.py
@@ -70,5 +70,5 @@ setup(
     python_requires='>3.6, !=3.7.0, !=3.7.1, !=3.7.2',
     test_suite='tests',
     extras_require=extras,
-    version=version.__version__,
+    version=version.__version__
 )
