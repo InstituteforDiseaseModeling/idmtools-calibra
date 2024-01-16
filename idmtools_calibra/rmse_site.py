22c3c8b9214d4ea5f60a8744f1f4785bbfab5c88
zdu <zdu@idmod.org>
2024-01-16 09:27:30 -0800

    Rename site name from RMSESiteSingleChannel to RMSESingleSite

--- idmtools_calibra/rmse_site.py
+++ idmtools_calibra/rmse_site.py
@@ -7,7 +7,7 @@ from idmtools_calibra.analyzers.rmse_analyzer import RMSEAnalyzer
 # analyzers will be used for scoring our calibration simulations.
 
 
-class RMSESiteSingleChannel(CalibSite):
+class RMSESingleSite(CalibSite):
     def __init__(self, name, reference_sources={'just_one': 'reference/output.csv'}):
         one_key = list(reference_sources.keys())[0]
         self.reference = pd.read_csv(reference_sources[one_key])
