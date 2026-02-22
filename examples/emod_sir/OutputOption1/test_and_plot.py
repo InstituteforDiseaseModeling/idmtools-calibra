2744fd1df42d33a700b61df6588b7ab51cc52976
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2026-02-21 17:50:54 -0800

    Fix example for emod_sir OutputOption1

--- examples/emod_sir/OutputOption1/test_and_plot.py
+++ examples/emod_sir/OutputOption1/test_and_plot.py
@@ -1,31 +1,16 @@
 import json
-from emodpy.emod_task import EMODTask
 from idmtools.entities.experiment import Experiment
 from idmtools.builders import SimulationBuilder
 import manifest 
 from emodpy.emod_task import EMODTask
 from settings import Settings
 import matplotlib
+import emod_api.campaign as camp
 #matplotlib.use( "TkAgg" )
 
 settings = Settings()
 
 # NOTE: Any campaign parameter you want to calibrate must be in the build_camp param list
-def build_camp():
-    """
-    Build a campaign input file for the DTK using emod_api. 
-    """
-    import emod_api.campaign as camp
-    import emod_api.interventions.outbreak as ob 
-
-    camp.set_schema( manifest.schema_file )
-
-    # Seed the outbreak
-    event = ob.new_intervention( camp, timestep=1, cases=1 )
-    camp.add( event )
-
-    return camp
-
 def get_task( build_camp_fn=None ):
     def set_param_fn( config ):
         config.parameters.Simulation_Duration = 365.0
@@ -39,20 +24,30 @@ def get_task( build_camp_fn=None ):
 
         return config
 
+    def build_camp(camp):
+        from emodpy.campaign.individual_intervention import OutbreakIndividual as OutbreakIndividual
+        from emodpy.campaign.common import TargetDemographicsConfig
+        from emodpy.campaign.distributor import add_intervention_scheduled
+        outbreak_event = OutbreakIndividual(campaign=camp)
+        target_demographics_config = TargetDemographicsConfig(demographic_coverage=0.4)
+        add_intervention_scheduled(camp,
+                                   intervention_list=[outbreak_event],
+                                   start_day=1,
+                                   target_demographics_config=target_demographics_config)
+
+        return camp
+
     import emod_generic.bootstrap as dtk
     dtk.setup( manifest.model_dl_dir )
     from idmtools.core.platform_factory import Platform
     platform = Platform(settings.LOCALE, node_group="idm_48cores", priority="AboveNormal")
-    task = EMODTask.from_default2(
-        config_path="config.json",
-        eradication_path=settings.MODEL_DRIVER,
-        campaign_builder=build_camp,
-        demog_builder=None,
-        schema_path=manifest.schema_file,
-        param_custom_cb=set_param_fn,
-        ep4_path=manifest.ep4
-    )
-    task.set_sif( settings.SIF )
+    task = EMODTask.from_defaults(eradication_path=settings.MODEL_DRIVER,
+                                  campaign_builder=build_camp,
+                                  schema_path=manifest.schema_file,
+                                  config_builder=set_param_fn,
+                                  embedded_python_scripts_path=manifest.ep4,
+                                  demographics_builder=None)
+    task.set_sif( settings.SIF, platform )
     return task, platform
 
 def test_and_plot():
